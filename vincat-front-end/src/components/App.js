b6abb823321500787b334904d8b88d7091afae63
Nicolas Mateo Casas Ramos <nmcasasr@unal.edu.co>
2019-05-17 04:30:55 -0500

    Tentative_navigationBar
    I try.

--- vincat-front-end/src/components/App.js
+++ vincat-front-end/src/components/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from '../assets/logo.svg';
+import React,{Component} from 'react';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import '../styles/App.css';
+import {homePage} from "./homePage";
+import {registerPage} from "./registerPage";
+import {loginPage} from "./loginPage";
+import {navigationBar} from "./navigationBar";
+import MainComponent from "./MainComponent";
+import {Layout} from "./Layout";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component
+{
+  render()
+  {
+    return (
+
+        <React.Fragment>
+
+            <Router>
+                {navigationBar()}
+                <Layout>
+            <Switch>
+              <Route exact path ="/" component={homePage}/>
+              <Route  path ="/login" component={loginPage}/>
+              <Route  path ="/register" component={registerPage}/>
+            </Switch>
+                </Layout>
+          </Router>
+
+
+        </React.Fragment>
+
+    );
+  }
+
 }
 
 export default App;
---
+++ vincat-front-end/src/components/homePage.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+import '../styles/App.css';
+import {navigationBar} from "./navigationBar";
+
+export const homePage = () => (
+
+<div><h1>Hola  sdasd</h1></div>
+
+
+)
+
+
+export default homePage;
