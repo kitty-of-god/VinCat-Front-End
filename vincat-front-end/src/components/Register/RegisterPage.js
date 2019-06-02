eb2f2ae73f675b5aa70d10c17f63513afb0b4eaa
Nicolas Mateo Casas Ramos <nmcasasr@unal.edu.co>
2019-06-01 22:18:46 -0500

    Add Sell and register

--- vincat-front-end/src/components/Register/RegisterPage.js
+++ vincat-front-end/src/components/Register/RegisterPage.js
@@ -1,11 +1,59 @@
 import React,{Component} from 'react';
 import '../../styles/App.css';
 import {Button, ButtonToolbar, Card, Col, Form, Row, Container} from "react-bootstrap";
+import axios from "axios";
+
+
+
 
 class RegisterPage extends Component{
+    constructor(props){
+        super(props);
 
+        this.handleChange = this.handleChange.bind(this);
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    }
+
+    handleChange(e){
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
+    handleFormSubmit(e){
+        e.preventDefault();
+
+        const users = {
+            email: this.state.email,
+            password: this.state.password,
+            password_confirmation: this.state.password_confirmation,
+            username: this.state.username,
+            role:"natural",
+            name: this.state.name
+        };
+        axios.post('https://vincat-dangulos.c9users.io/users', {users}
+
+            )
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+            }).catch(error => {
+
+            console.log(error.response.data.name)
+            console.log(error.response.data.email)
+            console.log(error.response.data.password)
+            console.log(error.response.data.password_confirmation)
+            console.log(error.response.data.username)
+
+
+        });
+
+        console.log(users);
+    }
   render(){
+      console.log(this.state);
     return(
+
       <Container style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
 
 
@@ -24,30 +72,36 @@ class RegisterPage extends Component{
               </Card.Header>
 
               <Card.Body >
-                  <Form className="justify-content-md-center">
+                  <Form className="justify-content-md-center" onSubmit={this.handleFormSubmit}>
                       <Form.Group as={Row} controlId="formHorizontalEmail" className="justify-content-md-center">
 
                           <Col sm={7}>
-                              <Form.Control type="email" placeholder="Email*" />
+                              <Form.Control onChange={this.handleChange} type="email" placeholder="Email*" name="email"/>
                           </Col>
                       </Form.Group>
 
                       <Form.Group as={Row} controlId="formHorizontalUsername" className="justify-content-md-center">
 
                           <Col sm={7}>
-                              <Form.Control type="username" placeholder="Username*" />
+                              <Form.Control onChange={this.handleChange} type="username" placeholder="Username*" name="username" />
+                          </Col>
+                      </Form.Group>
+                      <Form.Group as={Row} controlId="formHorizontalUsername" className="justify-content-md-center">
+
+                          <Col sm={7}>
+                              <Form.Control onChange={this.handleChange} type="name" placeholder="Name*" name="name" />
                           </Col>
                       </Form.Group>
                       <Form.Group as={Row} controlId="formHorizontalPassword" className="justify-content-md-center">
 
                           <Col sm={7}>
-                              <Form.Control type="password" placeholder="Password*" />
+                              <Form.Control onChange={this.handleChange} type="password" placeholder="Password*" name="password"/>
                           </Col>
                       </Form.Group>
                       <Form.Group as={Row} controlId="formHorizontalPassword" className="justify-content-md-center">
 
                           <Col sm={7}>
-                              <Form.Control type="password" placeholder="Re-type Password*" />
+                              <Form.Control onChange={this.handleChange} type="password" placeholder="Re-type Password*" name="password_confirmation"/>
                           </Col>
                       </Form.Group>
                       <Form.Group as={Row} className="justify-content-md-center">
---
+++ vincat-front-end/src/components/Sell/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import SellPage from "./SellPage";
+
+class Sell extends Component{
+    render(){
+        return(
+            <div className="Sell">
+                <SellPage/>
+            </div>
+        );
+    }
+}
+
+export default Sell;
